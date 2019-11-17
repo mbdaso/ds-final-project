0dd7d35d1691d03e98f5c1788078ade71d6c9138
Martin Belda <alu0100832211@ull.edu.es>
2019-11-17 20:31:52 +0100

    First commit

---
+++ src/Consumer.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+
+
+public class Consumer {
+
+	KafkaConsumer<String, String> consumer;
+	//The class KafkaConsumer creates a consumer for a group of consumer threads.
+	Consumer(){
+		Properties props = new Properties();
+		//The property BOOTSTRAP_SERVERS_CONFIG is the same as the producer properties.
+		props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "Group1");
+		props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+		StringDeserializer.class.getName());
+		props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+		StringDeserializer.class.getName());
+
+		consumer = new KafkaConsumer<>(props);
+	}
+	public void subscribe(List<String> topics){
+		consumer.subscribe(topics);
+	}
+	
+	public void consumeForEver() {
+		while (true) {
+			//System.out.println("Start polling");
+			ConsumerRecords<String, String> records = consumer.poll(100);
+//			System.out.println("Exit polling");
+			for (ConsumerRecord<String, String> record : records) { 
+				System.out.println("something consumed");
+				System.out.printf("offset = %d, key = %s, value = %s%n",
+				record.offset(), record.key(), record.value());
+			}//for
+		} //while
+	}
+	
+	public static void main(String[] args) {
+		List<String> topics = Arrays.asList("userId", "City");
+		Consumer myconsumer = new Consumer();
+		
+		myconsumer.subscribe(topics);
+		myconsumer.consumeForEver();
+	}	
+}
---
+++ src/SimpleProducer.java
@@ -0,0 +1,37 @@
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class SimpleProducer {
+	Properties props;
+	// The producer is a Kafka client that publishes records to the Kafka
+	// cluster.
+	KafkaProducer<String, String> producer;
+	SimpleProducer() {
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092");
+		// Serializer for conversion the key type to bytes
+		props.put("key.serializer",
+				"org.apache.kafka.common.serialization.StringSerializer");
+		// Serializer for conversion the value type to bytes
+		props.put("value.serializer",
+				"org.apache.kafka.common.serialization.StringSerializer");
+		producer = new KafkaProducer<>(props);
+	}
+	void produceAndPrint() {
+		for (int i = 1; i < 100; i++)
+			// Fire-and-forget send(topic, key, value)
+			// Send adds records to unsent records buffer and return
+			producer.send(new ProducerRecord<String, String>("SDTF", Integer
+					.toString(i), Integer.toString(i)));
+	}
+	void stop() {
+		producer.close();
+	}
+	public static void main(String[] args) {
+		SimpleProducer myProducer = new SimpleProducer();
+		myProducer.produceAndPrint();
+		myProducer.stop();
+		System.out.println("End");
+	}
+}
